8c9f0507eff7ff4967a2b4ca9169efc831a909b9
Serchcode <pe280591@uaeh.edu.mx>
2016-12-15 17:07:17 -0600

    frontend y back

--- Clientes/urls.py
+++ Clientes/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login,logout
+from django.contrib.auth.views import login,logout,logout_then_login
 from . import views
 
 urlpatterns = [
@@ -7,5 +7,7 @@ urlpatterns = [
 	url(r'^registros/$', views.Registros.as_view(), name="registros"),
 	url(r'^registros/(?P<id>\d+)$', views.Detalle.as_view(), name="detalle"),
 	url(r'^cerrar/(?P<id>\d+)$', views.Cerrar.as_view(), name="cerrar"),
-	url(r'^eliminar/(?P<id>\d+)$', views.Borrar.as_view(), name="eliminar")
+	url(r'^eliminar/(?P<id>\d+)$', views.Borrar.as_view(), name="eliminar"),
+	url(r'^logout/$', logout, name="logout"),
+	url(r'^logout-then-login/$', logout_then_login, name="logout_then_login"),
 ]
