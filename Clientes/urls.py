9f2db3c3e31bfdaca6d0c5e1b71498b39f77b733
Serchcode <pe280591@uaeh.edu.mx>
2016-12-15 19:05:03 -0600

    urls

--- Clientes/urls.py
+++ Clientes/urls.py
@@ -3,11 +3,11 @@ from django.contrib.auth.views import login,logout,logout_then_login
 from . import views
 
 urlpatterns = [
-	url(r'^login/$', login, name="login"),
 	url(r'^registros/$', views.Registros.as_view(), name="registros"),
 	url(r'^registros/(?P<id>\d+)$', views.Detalle.as_view(), name="detalle"),
 	url(r'^cerrar/(?P<id>\d+)$', views.Cerrar.as_view(), name="cerrar"),
 	url(r'^eliminar/(?P<id>\d+)$', views.Borrar.as_view(), name="eliminar"),
+	url(r'^login/$', login, name="login"),
 	url(r'^logout/$', logout, name="logout"),
 	url(r'^logout-then-login/$', logout_then_login, name="logout_then_login"),
 ]
