ac89a45a4e49b8884ea21ebdba1f5ad694687be3
MartinMelo97 <nitram-210397@hotmail.com>
2017-02-25 11:53:54 -0600

    Implementamos if en el settings para las bases de datos

--- Terrenofacil/settings.py
+++ Terrenofacil/settings.py
@@ -78,12 +78,32 @@ WSGI_APPLICATION = 'Terrenofacil.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+if DEBUG:
+
+    DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       }
     }
-}
+else:
+    DATABASES = {
+        'default':{
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME':'terrenofacil',
+            'USER':'root',
+            'PASSWORD':'#Poweroso77',
+            'HOST':'localhost',
+            'PORT':'',
+        }
+    }
 
 
 # Password validation
